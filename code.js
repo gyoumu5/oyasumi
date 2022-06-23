ea353f057f8be38f9e0c0150c080bfcfbaa94b6d
Glitch (glitch-hello-node) <none>
2022-06-23 02:25:42 +0000

    📇🌯 Checkpoint ./code.js:37072258/800 ./package.json:37072258/3

--- code.js
+++ code.js
@@ -1,19 +1,18 @@
-const {Client} = require("discord.js")
-const options = {intents:["GUILDS","GUILD_MESSAGES"]}
+const { Client } = require("discord.js");
+const options = { intents: ["GUILDS", "GUILD_MESSAGES"] };
 const client = new Client(options);
 
-
-client.on("ready", message => {
+client.on("ready", (message) => {
   console.log("Bot準備完了！");
 });
 
 //ここから
 
-client.on("messageCreate", message =>{
-  if (message.content === "hello."){
-    message.channel.send(`hello! ${message.author}`)
+client.on("messageCreate", (message) => {
+  if (message.content === "hello.") {
+    message.channel.send(`hello! ${message.author}`);
   }
-})
+});
 
 //ここまで
 
