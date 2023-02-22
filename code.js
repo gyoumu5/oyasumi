5581b02364aa45fbbaa9d8cc03d404c885ee97ff
Glitch (glitch-hello-node) <none>
2023-02-22 09:09:25 +0000

    👼🍢 Checkpoint ./code.js:54985331/1983

--- code.js
+++ code.js
@@ -6,26 +6,33 @@ client.on("ready", (message) => {
   console.log("Bot準備完了！");
 });
 
-//ここから
-
-client.on("messageCreate", (message) => {
-  if (message.content === "hello.") {
-    message.channel.send(`hello! ${message.author}`);
-  }
-});
-
-//ここまで
-
 client.on('message', message =>{
   if (message.author.bot) return;
-  if (message.content.match("藤田勇")){　//AまたはBというキーワードでBOTを稼働させたい
-　let channel = message.channel;
-　let author = message.author.username;
-　let reply_text = `${message.content}`;
-  
-  client.channels.cache.get('1077445009160286288').send(reply_text) //メッセージを取得したチャンネルとは別のチャンネルDに、発言ユーザーとメッセージABCの全文を送信したい
+    if (message.content.match("藤田勇")){　//AまたはBというキーワードでBOTを稼働させたい
+    　let channel = message.channel;
+　    let author = message.author.username;
+　    let reply_text = `${message.content}`;
+      client.channels.cache.get('1077445009160286288').send(reply_text) //メッセージを取得したチャンネルとは別のチャンネルDに、発言ユーザーとメッセージABCの全文を送信したい
+    }
+    if (message.content.match("宮﨑亮")){　//AまたはBというキーワードでBOTを稼働させたい
+    　let channel = message.channel;
+　    let author = message.author.username;
+　    let reply_text = `${message.content}`;
+      client.channels.cache.get('1077444315774734438').send(reply_text) //メッセージを取得したチャンネルとは別のチャンネルDに、発言ユーザーとメッセージABCの全文を送信したい
+    }
+    if (message.content.match("大石彰久")){　//AまたはBというキーワードでBOTを稼働させたい
+    　let channel = message.channel;
+　    let author = message.author.username;
+　    let reply_text = `${message.content}`;
+      client.channels.cache.get('1077446655965663253').send(reply_text) //メッセージを取得したチャンネルとは別のチャンネルDに、発言ユーザーとメッセージABCの全文を送信したい    
+    }
+    if (message.content.match("大石彰久")){　//AまたはBというキーワードでBOTを稼働させたい
+    　let channel = message.channel;
+　    let author = message.author.username;
+　    let reply_text = `${message.content}`;
+      client.channels.cache.get('1077446655965663253').send(reply_text) //メッセージを取得したチャンネルとは別のチャンネルDに、発言ユーザーとメッセージABCの全文を送信したい    
+    }
   return;
-  }
 });
 
 
