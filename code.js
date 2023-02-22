97011a14ade356fdde590db03001ce756aebe1da
Glitch (glitch-hello-node) <none>
2023-02-22 05:47:26 +0000

    ✌️🎹 Checkpoint ./discord.js:54985331/830 ./code.js:54985331/430

--- code.js
+++ code.js
@@ -16,4 +16,17 @@ client.on("messageCreate", (message) => {
 
 //ここまで
 
+client.on('message', message =>{
+  if (message.author.bot) return;
+  if (message.content.match(/test|aaaa/)){　//AまたはBというキーワードでBOTを稼働させたい
+　let channel = message.channel;
+　let author = message.author.username;
+　let reply_text = `${author}が${message.content}と<#${message.channel.id}>で発言しています`;
+  
+  client.channels.cache.get('チャンネルID').send(reply_text) //メッセージを取得したチャンネルとは別のチャンネルDに、発言ユーザーとメッセージABCの全文を送信したい
+  return;
+  }
+});
+
+
 client.login(process.env.DISCORD_BOT_TOKEN);
