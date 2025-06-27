fd21858efa597060ece4dbe3dbfdd8649371e8e9
gyoumu5 <ikm.tok.gyoumu5@gmail.com>
2025-06-27 16:58:40 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const http = require("http");
 const querystring = require("node:querystring");
 
-//GASでwakeさせること。
+// Renderの場合はこのように修正！（3000固定をprocess.env.PORTへ）
+const PORT = process.env.PORT || 3000;
 
 http
   .createServer(function(req, res) {
@@ -26,14 +27,16 @@ http
       });
     } else if (req.method == "GET") {
       res.writeHead(200, { "Content-Type": "text/plain" });
-      res.end("Discord Bot is Oprateing!");
+      res.end("Discord Bot is Operating!");
     }
   })
-  .listen(3000);
+  .listen(PORT);  // ここを修正！！必ずprocess.env.PORTで！
 
-if (process.env.DISCORD_BOT_TOKEN == undefined || process.env.DISCORD_BOT_TOKEN == "") {
+console.log(`HTTPサーバーがポート${PORT}で起動しました`);
+
+if (!process.env.DISCORD_BOT_TOKEN) {
   console.log("DISCORD_BOT_TOKENを設定してください。");
   process.exit(0);
 }
 
-require("./code.js")
+require("./code.js");
